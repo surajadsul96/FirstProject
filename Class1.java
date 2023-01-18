66f2cd32c3eac57233361032389e6bc38e7adc47
surajadsul96 <122977724+surajadsul96@users.noreply.github.com>
2023-01-18 17:02:11 +0530

    Changes made by suraj

--- Class1.java
+++ Class1.java
@@ -2,5 +2,6 @@ public class Class1 {
     public static void main(String[] args) {
         System.out.println("First Git Project");
         System.out.println("Branch_Suraj");
+        System.out.println("Changes in code in GitHub");
     }
 }
